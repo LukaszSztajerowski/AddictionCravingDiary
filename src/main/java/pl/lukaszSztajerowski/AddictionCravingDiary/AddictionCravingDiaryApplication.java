dd048dcfd966dc898ba18b9388933d7c27856096
LukaszSztajerowski <lukaszsztajerowski@gmail.com>
2021-10-03 13:44:20 +0200

    init commit

---
+++ src/main/java/pl/lukaszSztajerowski/AddictionCravingDiary/AddictionCravingDiaryApplication.java
@@ -0,0 +1,13 @@
+package pl.lukaszSztajerowski.AddictionCravingDiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddictionCravingDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddictionCravingDiaryApplication.class, args);
+	}
+
+}
